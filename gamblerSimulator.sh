ab67e87e7c3c88184e424374a408c0642baa8b7f
Raj Gopal <harirajagopal393@gmail.com>
2020-04-14 22:10:59 +0530

    Added feature to compute earnings playing continously for 20 days

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -4,9 +4,25 @@ betPerGame=1
 gameStatus=$((RANDOM%2))
 isWin=1
 isLost=0
-if [ $isWin -eq $gameStatus ]
-	then 
-	   echo "You Won The Game this time"
-	else
-	   echo "You Lost The Game.."
-fi
+remainingMoney=0
+maxAmount=150
+minAmount=50
+for(( i=1; $i<=20; i++ ))
+	do
+		while(true)
+   		   do
+			if [ $isWin -eq $gameStatus ]
+			  then 
+	   			initialAmount=$(($initialAmount+1))
+			  else
+	   			initialAmount=$(($initialAmount-1))
+			fi
+			if [ $initialAmount -gt $maxAmount ] || [ $initialAmount -le $minAmount ]
+		   	  then
+				remainingMoney=$initialAmount
+				initialMoney=100
+				break;
+			fi
+   		   done
+	done
+echo "after 20 days remaining money is $remainingMoney"
